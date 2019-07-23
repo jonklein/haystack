e63196cb03e85c8d857cfe8814d12ad5cd06b1ea
Jonathan Klein <jk@kleininen.com>
2019-07-23 08:15:40 -0400

    working cli escript

--- lib/haystack.ex
+++ lib/haystack.ex
@@ -1,12 +1,19 @@
 defmodule Haystack do
   use Application
 
-  def start(_type, _args) do
-    IO.inspect("start")
+  def load_data(file) do
+    Poison.decode!(File.read!(file))
+  end
+
+  def main(args) do
+    data = load_data(Enum.at(args, 0))
+    IO.inspect(data)
 
     Task.start(fn ->
-      GP.build(Enum.map(1..50, fn i -> [i, 5*i*i + 2*i + 8] end), tournamentSize: 12, populationSize: 1200, individualSize: 40)
+      GP.build(data, tournamentSize: 12, populationSize: 1200, individualSize: 40)
         |> GP.steps(300)
     end)
+
+    :timer.sleep(:infinity)
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,24 +7,21 @@ defmodule Haystack.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: [main_module: Haystack]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: {Haystack, []}, 
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:poison, "~> 3.1"}
     ]
   end
 end
